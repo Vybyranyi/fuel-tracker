3034f0739000f762466263b550dcf3103b82ad1c
Vybyranyi <mariankvit@gmail.com>
2026-08-27 21:52:12 +0300

    feat: add dynamic site icon generation using Next.js ImageResponse

---
+++ src/app/icon.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'black',
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M14 13h2a2 2 0 0 1 2 2v2a2 2 0 0 0 4 0v-6.998a2 2 0 0 0-.59-1.42L18 5" />
+          <path d="M14 21V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v16" />
+          <path d="M2 21h13" />
+          <path d="M3 9h11" />
+        </svg>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      ...size,
+    }
+  )
+}
